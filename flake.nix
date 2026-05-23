b2f256a8651dbd26a546dbb24b094cf835879cd9
coding-seal <rodion2023@mail.ru>
2026-05-23 20:02:02 +0300

    add mirrors

--- flake.nix
+++ flake.nix
@@ -36,8 +36,14 @@
           # binaries instead of compiling them locally (saves ~30 min)
           {
             nix.settings = {
-              extra-substituters = [ "https://noctalia.cachix.org" ];
+              extra-substituters = [
+                "https://cache.nixos.org"
+                "https://nix-community.cachix.org"
+                "https://noctalia.cachix.org"
+              ];
               extra-trusted-public-keys = [
+                "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
                 "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
               ];
             };
