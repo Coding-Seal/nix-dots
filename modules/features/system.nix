7e9f590638030fd21926243825d68408e3b383df
r.ganiullin <r.ganiullin@NB-6223.lan>
2026-06-17 23:52:18 +0300

    add grub

--- modules/features/system.nix
+++ modules/features/system.nix
@@ -3,30 +3,34 @@
   config.nixosModules = [
     { nixpkgs.overlays = [ inputs.nur.overlays.default ]; }
     ({ pkgs, ... }: {
-      boot.loader.systemd-boot.enable = true;
-      boot.loader.efi.canTouchEfiVariables = true;
+      # Replace systemd-boot with GRUB for dual boot
+      boot.loader = {
+        efi.canTouchEfiVariables = true;
+        grub = {
+          enable = true;
+          device = "nodev";
+          efiSupport = true;
+          useOSProber = true;
+        };
+      };
 
       time.timeZone = "UTC";
       i18n.defaultLocale = "en_US.UTF-8";
-
       users.users.${config.username} = {
         isNormalUser = true;
         extraGroups = [ "wheel" "networkmanager" "video" "audio" "seat" ];
         shell = pkgs.fish;
       };
-
       nix.settings.experimental-features = [ "nix-command" "flakes" ];
       nixpkgs.config.allowUnfree = true;
-
       security.polkit.enable = true;
       security.rtkit.enable = true;
-
       zramSwap = {
         enable = true;
         algorithm = "zstd";
         memoryPercent = 50;
       };
-
+      environment.systemPackages = [ pkgs.os-prober ];
       system.stateVersion = "25.05";
     })
   ];
